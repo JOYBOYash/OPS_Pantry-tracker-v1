c9b59b19fd2c97d9146b06a9a541fe08fd09cafb
Akili P V S S SRI SWAROOP KUMAR <sriswaroop12005@gmail.com>
2024-08-26 01:13:15 +0530

    deployment bug fixes..

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
+module.exports = nextConfig;
